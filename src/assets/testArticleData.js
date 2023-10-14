1c1c82a2be994db587696fe6bad59c8cf0589151
边俣 <310734304@qq.com>
2023-10-14 10:42:04 +0800

    up

--- src/assets/testArticleData.js
+++ src/assets/testArticleData.js
@@ -384,6 +384,7 @@ const article = `
 
 {|albumIS
 |align=center
+|imgW=200px
 |-
 | https://img.moegirl.org.cn/common/thumb/6/6f/Toaru_Kagaku_no_Railgun_T_Anime_KV2.jpg/450px-Toaru_Kagaku_no_Railgun_T_Anime_KV2.jpg
 | https://img.moegirl.org.cn/common/thumb/e/e5/Misaka_-_BD2.jpg/375px-Misaka_-_BD2.jpg
