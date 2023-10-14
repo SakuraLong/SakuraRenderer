5b429b93aeb7b99586938c703e3c70f207b05524
NKUmianman <1277463846@qq.com>
2023-10-14 21:32:32 +0800

    解决center样式问题

--- src/assets/testArticleData.js
+++ src/assets/testArticleData.js
@@ -412,8 +412,8 @@ const article = `
 
 {|carouselIS
 |cycle
-|center
 |play
+|center
 |-
 | https://img.moegirl.org.cn/common/thumb/6/6f/Toaru_Kagaku_no_Railgun_T_Anime_KV2.jpg/450px-Toaru_Kagaku_no_Railgun_T_Anime_KV2.jpg
 | https://img.moegirl.org.cn/common/thumb/e/e5/Misaka_-_BD2.jpg/375px-Misaka_-_BD2.jpg
