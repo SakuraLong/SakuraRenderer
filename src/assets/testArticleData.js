f5d490cfa41704c3f8b552cbc4c5021075e38423
SakuraLong <1897111@qq.com>
2023-10-14 11:54:29 +0800

    update

--- src/assets/testArticleData.js
+++ src/assets/testArticleData.js
@@ -268,7 +268,7 @@ const article = `
 === 当然你也可以布局
 
 {|carouselIS
-| height=300px|right|play=3000
+| center|play=3000
 |-
 | https://img.moegirl.org.cn/common/thumb/6/6f/Toaru_Kagaku_no_Railgun_T_Anime_KV2.jpg/450px-Toaru_Kagaku_no_Railgun_T_Anime_KV2.jpg
 | https://img.moegirl.org.cn/common/thumb/e/e5/Misaka_-_BD2.jpg/375px-Misaka_-_BD2.jpg
