6598eb4ecf4fc46b9f4cc2b670651eaebc47d7d1
NKUmianman <1277463846@qq.com>
2023-12-13 11:31:43 +0800

    模版analyse修改

--- src/sakura-renderer/src/componentsParser/template/delParser.js
+++ src/sakura-renderer/src/componentsParser/template/delParser.js
@@ -39,20 +39,21 @@ class DelParser extends Template {
             }
         };
         this.dataList.forEach((data, index) => {
-            let key = data.split("=")[0];
-            let value = data.split("=")[data.split("=").length - 1];
+            const key = data.split("=")[0];
+            const left = data.indexOf("=");
+            const value = data.slice(left + 1, data.length);
             switch (index) {
                 case 1:
-                    if (key === value) text = value;
-                    else switchKeyValue(key, value);
+                    if (switchKeyValue(key, value)) break;
+                    else text = data;
                     break;
                 case 2:
-                    if (key === value) color = value;
-                    else switchKeyValue(key, value);
+                    if (switchKeyValue(key, value)) break;
+                    else color = data;
                     break;
                 case 3:
-                    if (key === value) size = value;
-                    else switchKeyValue(key, value);
+                    if (switchKeyValue(key, value)) break;
+                    else size = data;
                     break;
             }
         });
